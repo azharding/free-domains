a8db2e790d684a571bd1697d05e3c29ddedd4b1a
azharding <97221932+azharding@users.noreply.github.com>
2024-08-04 09:04:58 +0700

    Create localku.is-an.app.js

---
+++ domains/localku.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+    description: 'A website to generate code screenshots', 
+    domain: 'is-an.app', 
+    subdomain: 'localku', 
+    owner: {
+      email: 'websitehosting21@gmail.com',
+    },
+    record: {
+      CNAME: 'cname.vercel-dns.com', 
+    },
+   
+  })
